8b4ceb74557890a588f4d8af3d25cff5ad330302
Gilmar <gilmar.jesus@gmail.com>
2022-02-01 21:43:11 -0300

    Criado tela com circulo no centro

---
+++ pacman/pacman.py
@@ -0,0 +1,18 @@
+import pygame
+
+AMARELO = (255, 255, 0)
+pygame.init()
+
+tela = pygame.display.set_mode((640, 480), 0)
+
+while True:
+    # Calcula as regras
+
+    # Pinta
+    pygame.draw.circle(tela, AMARELO, (320, 240), 50, 0)
+    pygame.display.update()
+
+    # Eventos
+    for acao in pygame.event.get():
+        if acao.type == pygame.QUIT:
+            exit()
