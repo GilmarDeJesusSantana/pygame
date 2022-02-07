9bf1f832b492eaeff95c513cab23a5c1161ead59
Gilmar <gilmar.jesus@gmail.com>
2022-02-07 16:02:43 -0300

    Implementado acao de comer as pastilhas

--- pacman/pacman_2.py
+++ pacman/pacman_2.py
@@ -8,8 +8,10 @@ screen = pygame.display.set_mode((800, 600), 0)
 
 
 class Cenario:
-    def __init__(self, tamanho):
+    def __init__(self, tamanho, pac):
+        self.pacman = pac
         self.tamanho = tamanho
+        self.pontos = 0
         self.matriz = [
             [2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2],
             [2, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2, 2, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 5, 2],
@@ -60,6 +62,17 @@ class Cenario:
         for numero_linha, linha in enumerate(self.matriz):
             self.pintar_linha(tela, numero_linha, linha)
 
+    def calcular_regras(self):
+        col = self.pacman.coluna_intencao
+        lin = self.pacman.linha_intencao
+        if 0 <= col < 28 and 0 <= lin < 29:
+            if self.matriz[lin][col] != 2:
+                self.pacman.aceitar_movimento()
+                if self.matriz[lin][col] == 1:
+                    self.pontos += 1
+                    self.matriz[lin][col] = 0
+                    print(self.pontos)
+
 
 class Pacman:
     def __init__(self, tamanho):
@@ -71,10 +84,12 @@ class Pacman:
         self.raio = self.tamanho // 2
         self.velocidade_x = 0
         self.velocidade_y = 0
+        self.coluna_intencao = self.coluna
+        self.linha_intencao = self.linha
 
     def calcula_regras(self):
-        self.coluna += self.velocidade_x
-        self.linha += self.velocidade_y
+        self.coluna_intencao += self.velocidade_x
+        self.linha_intencao += self.velocidade_y
         self.centro_x = int(self.coluna * self.tamanho + self.raio)
         self.centro_y = int(self.linha * self.tamanho + self.raio)
 
@@ -98,45 +113,50 @@ class Pacman:
         olho_raio = int(self.raio / 10)
         pygame.draw.circle(tela, PRETO, (olho_x, olho_y), olho_raio, 0)
 
-    def processar_eventos(self, eventos):
-        for acao in eventos:
-            if acao.type == pygame.KEYDOWN:
-                if acao.key == pygame.K_RIGHT:
+    def processar_eventos(self, eventos_key):
+        for acoes in eventos_key:
+            if acoes.type == pygame.KEYDOWN:
+                if acoes.key == pygame.K_RIGHT:
                     self.velocidade_x = VELOCIDADE
-                elif acao.key == pygame.K_LEFT:
+                elif acoes.key == pygame.K_LEFT:
                     self.velocidade_x = -VELOCIDADE
-                elif acao.key == pygame.K_UP:
+                elif acoes.key == pygame.K_UP:
                     self.velocidade_y = -VELOCIDADE
-                elif acao.key == pygame.K_DOWN:
+                elif acoes.key == pygame.K_DOWN:
                     self.velocidade_y = VELOCIDADE
 
-            elif acao.type == pygame.KEYUP:
-                if acao.key == pygame.K_RIGHT:
+            elif acoes.type == pygame.KEYUP:
+                if acoes.key == pygame.K_RIGHT:
                     self.velocidade_x = PARAR
-                elif acao.key == pygame.K_LEFT:
+                elif acoes.key == pygame.K_LEFT:
                     self.velocidade_x = PARAR
-                elif acao.key == pygame.K_UP:
+                elif acoes.key == pygame.K_UP:
                     self.velocidade_y = PARAR
-                elif acao.key == pygame.K_DOWN:
+                elif acoes.key == pygame.K_DOWN:
                     self.velocidade_y = PARAR
 
-    def processar_eventos_mouse(self, eventos):
+    def processar_eventos_mouse(self, eventos_mouse):
         delay = 100
-        for ponteiro in eventos:
+        for ponteiro in eventos_mouse:
             if ponteiro.type == pygame.MOUSEMOTION:
                 mouse_x, mouse_y = ponteiro.pos
                 self.coluna = (mouse_x - self.centro_x) / delay
                 self.linha = (mouse_y - self.centro_y) / delay
 
+    def aceitar_movimento(self):
+        self.coluna = self.coluna_intencao
+        self.linha = self.linha_intencao
+
 
 if __name__ == '__main__':
     size = 600 // 30
     pacman = Pacman(size)
-    cenario = Cenario(size)
+    cenario = Cenario(size, pacman)
 
     while True:
         # Calcular as regras
         pacman.calcula_regras()
+        cenario.calcular_regras()
 
         # Pintar a tela
         screen.fill(PRETO)
